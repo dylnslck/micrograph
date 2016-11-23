19090b94b36519bb74f7587ff828ece967830fea
Dylan <dylanslack@gmail.com>
2016-11-22 22:34:15 -0700

    Ensure that attributes are valid leaf types

--- src/buildAttributes.js
+++ src/buildAttributes.js
@@ -1,10 +1,21 @@
-export default (type) => {
-  const { attributes } = type;
+import { isLeafType } from 'graphql';
 
-  return attributes.reduce((prev, { field, type: attrType }) => ({
-    ...prev,
-    [field]: {
-      type: attrType,
-    },
-  }), {});
+export default (type) => {
+  const { name, attributes } = type;
+
+  return attributes.reduce((prev, { field, type: attrType }) => {
+    if (!isLeafType(attrType)) {
+      throw new TypeError(
+        `The ${field} attribute of the ${name} type is not a valid GraphQL leaf type. All ` +
+        'attributes must be a valid leaf type'
+      );
+    }
+
+    return {
+      ...prev,
+      [field]: {
+        type: attrType,
+      },
+    };
+  }, {});
 };
