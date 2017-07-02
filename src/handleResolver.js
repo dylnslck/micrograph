b23b48e08e531909d6db66ffcefd9c8dc8ba2904
Dylan Slack <dylanslack@gmail.com>
2017-07-01 18:02:29 -0600

    Add ast to resolve argument list

--- src/handleResolver.js
+++ src/handleResolver.js
@@ -1,8 +1,8 @@
-export default (args, ctx, resolver, middleware) => {
+export default (args, ctx, ast, resolver, middleware) => {
   const { handle, finalize, error } = resolver;
   const stack = middleware && middleware.stack;
 
-  return new Promise((resolve, reject) => handle(stack, args, ctx, resolve, reject))
+  return new Promise((resolve, reject) => handle(stack, args, ctx, ast, resolve, reject))
     .then(({ finalArgs, finalCtx }) => finalize(finalArgs, finalCtx))
     .catch(error);
 };
